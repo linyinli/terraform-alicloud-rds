b540225ed0d9cd691bc068dc511a5de092a4f88f
Yinlin Li <liyinlin926@gmail.com>
2023-04-13 18:20:07 +0800

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -5,9 +5,9 @@ locals {
   create_more_resources = var.create_instance
   create_database       = local.create_more_resources && var.create_database
   create_account        = local.create_more_resources && var.create_account
-  security_group_ids    = length(var.security_group_ids) > 0 ? var.security_group_ids : var.vpc_security_group_ids
-  retention_period      = var.backup_retention_period != 7 ? var.backup_retention_period : var.retention_period
-  log_retention_period  = var.log_backup_retention_period != 7 ? var.log_backup_retention_period : var.log_retention_period
-  backup_time           = length(var.preferred_backup_time) > 0 ? var.preferred_backup_time : var.backup_time
-  backup_period         = length(var.preferred_backup_period) > 0 ? var.preferred_backup_period : var.backup_period
+  security_group_ids    = length(var.security_group_ids) > 0 ? var.security_group_ids : var.security_group_ids
+  retention_period      = var.backup_retention_period != 7 ? var.backup_retention_period : var.backup_retention_period
+  log_retention_period  = var.log_backup_retention_period != 7 ? var.log_backup_retention_period : var.log_backup_retention_period
+  backup_time           = length(var.preferred_backup_time) > 0 ? var.preferred_backup_time : var.preferred_backup_time
+  backup_period         = length(var.preferred_backup_period) > 0 ? var.preferred_backup_period : var.preferred_backup_period
 }
